dd18a3d32e404c2843b95fab010fb9d4b48f32b3
JoeRutland <joe@jrutlanddesign.co.uk>
2016-07-28 13:13:19 +0100

    JS for navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+console.log('Website design by Joe Rutland - http://joerutland.com');
+
+// resposive navigation
+$(document).ready(function(){
+    $(".navTrigger").click(function(){
+        $("nav").toggleClass("open");
+    });
+});
