ad3d9d1c2d93e6d0ed0393dd2c4c00563e3fec5a
JoeRutland <joe@jrutlanddesign.co.uk>
2016-07-28 17:06:19 +0100

    style altes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ console.log('Website design by Joe Rutland - http://joerutland.com');
 
 // resposive navigation
 $(document).ready(function(){
-    $(".navTrigger").click(function(){
-        $("nav").toggleClass("open");
+    $('.navTrigger').click(function(){
+        $('nav').toggleClass('open');
     });
 });
