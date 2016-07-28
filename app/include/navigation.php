ee19312a292821f30e6a1748785b09789839a229
JoeRutland <joe@jrutlanddesign.co.uk>
2016-07-28 16:23:13 +0100

    convert to php

---
+++ app/include/navigation.php
@@ -0,0 +1,7 @@
+<ul>
+	<a href="#"><li>Mortgages</li></a>
+    <a href="#"><li>Life Assurance</li></a>
+    <a href="#"><li>Protection</li></a>
+    <a href="#"><li>Home Insurance</li></a>
+    <a href="#"><li>Contact Us</li></a>
+</ul>
---
+++ app/index.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Suttons Mortgage Brokers - mortgage and home insurance brokers in Yorkshire</title>
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- build:css styles/main.css -->
+    <link rel="stylesheet" href="styles/main.css">
+    <!-- endbuild -->
+    
+    <!-- build:js scripts/vendor/modernizr.js -->
+    <script src="/bower_components/modernizr/modernizr.js"></script>
+    <!-- endbuild -->
+  </head>
+  <body>
+    <!--[if lt IE 10]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <header>
+      <img src="images/suttons-mortgage-brokers.png" alt="Suttons Mortgage Brokers, Yorkshire" class="logo">
+      <div id="topContact">
+        <img src="images/phone-icon.svg" alt="contact Suttons Mortgage Brokers, Yorkshire">
+        <span>01482 376245</span>
+      </div>
+
+      <a href="#" id="menuIcon" class="navTrigger"><img src="images/nav-icon.svg" alt="menu icon - click to expand menu"></a>
+    </header>
+
+    <nav>
+      <?php inclue('include/navigation.php'); ?>
+    </nav>
+
+    <div class="container">
+
+      <section id="heroSlider">
+        <img src="images/couple-getting-mortgage-advice.jpg" alt="A young couple getting advice form a mortgage broker in  Yorkshire">
+      </section>
+
+      <section class="titleContain">
+        <h1>All Types of Mortgages Arranged</h1>
+      </section>
+
+      <section id="services">
+
+        <div class="icon" id="advice">
+          <img src="images/checklist-icon.svg" alt="icon of a mortgage checklist">
+          <h2>Whole Market Advice</h2>
+        </div>
+
+        <div class="icon" id="local">
+          <img src="images/location-icon-yorkshire.svg" alt="map location icon - close to you in Yorkshire">
+          <h2>Local to You</h2>
+        </div>
+
+        <div class="icon" id="face-to-face">
+          <img src="images/handshake-icon.svg" alt="handshake icon - getting mortgage advice face to face or over the phone">
+          <h2>Face-to-face &amp; Telephone Advice</h2>
+        </div>
+
+        <div class="icon" id="holistic">
+          <img src="images/graphs-icon.svg" alt="icon of a chart with graphs showing the planning of your finances">
+          <h2>A Holistic Approach to Financial Planning</h2>
+        </div>
+      </section>
+
+      <article>
+        <p>We are <span class="boldItalic">not</span> tied to one single company, or a panel of insurers, and are able to use the latest technology to search the market on your behalf and find competitive mortgage, life insurance, critical illness, income protection plans to suit your needs.</p>
+      </article>
+
+      <section id="warning">
+        Your home may be reprosessed if you don't keep up repayments on your mortgage
+      </section>
+
+      <footer>
+        <div class="twentyFive">
+          <b>Registered Office:</b>
+          <address>
+          22 Highfield, Sutton, Hull, 
+          East Yorkshire,
+          HU7 4TP.
+          </address>
+
+
+          <b>Registered in England number:</b>
+          07484503
+
+          <div id="social">
+            <img src="images/facebook.svg" alt="Like Suttons Morgage Brokers on Facebook">
+            <img src="images/twitter.svg" alt="Follow Suttons Morgages Brokers of Yorkshire on Twitter">
+            <img src="images/linkedin.svg" alt="Connect with Suttons Morgage Brokers on LinkedIn">
+            <img src="images/google-plus.svg" alt="Join Suttons Morgage Brokers on Google+">
+          </div>
+        </div>
+        <!-- // END quarter -->
+
+        <div class="seventyFive">
+          <p>We do not normally charge a fee for initial mortgage advice, however this will be dependent on your circumstances. If a fee is charged our typical fee is <b>£199</b>.</p> 
+
+          <p>Sutton Mortgage Brokers Limted is Authorised and Regulated by the Financial Conduct Authority; our registration number is 543278.</p>
+
+          <p>Some of the products/services shown above are not or may not be regulated by the Financial Conduct Authority.</p>
+
+          <p>The guidance and/or advice contained within this website is subject to the UK regulatory regime, and is therefore targeted at consumers based in the UK.</p>
+        </div>
+        <!-- // END seventyFive -->
+
+        <div class="lower">
+          <p>&copy; Copyright, Sutton Mortgage Brokers Limited <?php date('Y'); ?></p>
+          <p><a href="http://jrutlanddesign.co.uk">UI Design</a> by J Rutland Design</p>
+        </div>
+
+      </footer>
+    </div><!--//END container -->
+    
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='https://www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+
+    <!-- build:js scripts/vendor.js -->
+    <!-- bower:js -->
+    <script src="/bower_components/jquery/dist/jquery.js"></script>
+    <!-- endbower -->
+    <!-- endbuild -->
+    
+    <!-- build:js scripts/main.js -->
+    <script src="scripts/main.js"></script>
+    <!-- endbuild -->
+  </body>
+</html>
