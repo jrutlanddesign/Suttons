d43256d274eb6170d192dbed013e3a820373cc25
JoeRutland <joe@jrutlanddesign.co.uk>
2016-07-28 17:07:51 +0100

    php includes

---
+++ app/include/footer.php
@@ -0,0 +1,65 @@
+<section id="warning">
+        Your home may be reprosessed if you don't keep up repayments on your mortgage
+      </section>
+
+      <footer>
+
+<div class="twentyFive">
+          <b>Registered Office:</b>
+          <address>
+          22 Highfield, Sutton, Hull, 
+          East Yorkshire,
+          HU7 4TP.
+          </address>
+
+
+          <b>Registered in England number:</b>
+          07484503
+
+          <div id="social">
+            <img src="images/facebook.svg" alt="Like Suttons Morgage Brokers on Facebook">
+            <img src="images/twitter.svg" alt="Follow Suttons Morgages Brokers of Yorkshire on Twitter">
+            <img src="images/linkedin.svg" alt="Connect with Suttons Morgage Brokers on LinkedIn">
+            <img src="images/google-plus.svg" alt="Join Suttons Morgage Brokers on Google+">
+          </div>
+        </div>
+        <!-- // END quarter -->
+
+        <div class="seventyFive">
+          <p>We do not normally charge a fee for initial mortgage advice, however this will be dependent on your circumstances. If a fee is charged our typical fee is <b>£199</b>.</p> 
+
+          <p>Sutton Mortgage Brokers Limted is Authorised and Regulated by the Financial Conduct Authority; our registration number is 543278.</p>
+
+          <p>Some of the products/services shown above are not or may not be regulated by the Financial Conduct Authority.</p>
+
+          <p>The guidance and/or advice contained within this website is subject to the UK regulatory regime, and is therefore targeted at consumers based in the UK.</p>
+        </div>
+        <!-- // END seventyFive -->
+
+        <div class="lower">
+          <p>&copy; Copyright, Sutton Mortgage Brokers Limited <?php date('Y'); ?></p>
+          <p><a href="http://jrutlanddesign.co.uk">UI Design</a> by J Rutland Design</p>
+        </div>
+
+      </footer>
+    </div><!--//END container -->
+    
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+      (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+      e.src='https://www.google-analytics.com/analytics.js';
+      r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+      ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+
+    <!-- build:js scripts/vendor.js -->
+    <!-- bower:js -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <!-- endbower -->
+    <!-- endbuild -->
+    
+    <!-- build:js scripts/main.js -->
+    <script src="scripts/main.js"></script>
+    <!-- endbuild -->
---
+++ app/include/head.php
@@ -0,0 +1,10 @@
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- build:css styles/main.css -->
+    <link rel="stylesheet" href="styles/main.css">
+    <!-- endbuild -->
+    
+    <!-- build:js scripts/vendor/modernizr.js -->
+    <script src="/bower_components/modernizr/modernizr.js"></script>
+    <!-- endbuild -->
