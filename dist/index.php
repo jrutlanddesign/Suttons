5775f2189367beee2362e46a3c0c81ee0983070b
JoeRutland <joe@jrutlanddesign.co.uk>
2016-07-28 17:08:03 +0100

    list files ready

---
+++ dist/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Suttons Mortgage Brokers - mortgage and home insurance brokers in Yorkshire</title>
+
+    <?php include('include/head.php'); ?>
+    
+  </head>
+  <body>
+    <!--[if lt IE 10]>
+      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <header>
+      <img src="images/suttons-mortgage-brokers.png" alt="Suttons Mortgage Brokers, Yorkshire" class="logo">
+      <div id="topContact">
+        <img src="images/phone-icon.svg" alt="contact Suttons Mortgage Brokers, Yorkshire">
+        <span>01482 376245</span>
+      </div>
+
+      <a href="#" id="menuIcon" class="navTrigger"><img src="images/nav-icon.svg" alt="menu icon - click to expand menu"></a>
+    </header>
+
+    <nav>
+      <?php include('include/navigation.php'); ?>
+    </nav>
+
+    <div class="container">
+
+      <section id="heroSlider">
+        <img src="images/couple-getting-mortgage-advice.jpg" alt="A young couple getting advice form a mortgage broker in  Yorkshire">
+      </section>
+
+      <section class="titleContain">
+        <h1>All Types of Mortgages Arranged</h1>
+      </section>
+
+      <section id="services">
+
+        <div class="icon" id="advice">
+          <img src="images/checklist-icon.svg" alt="icon of a mortgage checklist">
+          <h2>Whole Market Advice</h2>
+        </div>
+
+        <div class="icon" id="local">
+          <img src="images/location-icon-yorkshire.svg" alt="map location icon - close to you in Yorkshire">
+          <h2>Local to You</h2>
+        </div>
+
+        <div class="icon" id="face-to-face">
+          <img src="images/handshake-icon.svg" alt="handshake icon - getting mortgage advice face to face or over the phone">
+          <h2>Face-to-face &amp; Telephone Advice</h2>
+        </div>
+
+        <div class="icon" id="holistic">
+          <img src="images/graphs-icon.svg" alt="icon of a chart with graphs showing the planning of your finances">
+          <h2>A Holistic Approach to Financial Planning</h2>
+        </div>
+      </section>
+
+      <article>
+        <p>We are <span class="boldItalic">not</span> tied to one single company, or a panel of insurers, and are able to use the latest technology to search the market on your behalf and find competitive mortgage, life insurance, critical illness, income protection plans to suit your needs.</p>
+      </article>
+
+        <?php include('include/footer.php'); ?>
+  </body>
+</html>
---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+console.log("Website design by Joe Rutland - http://joerutland.com"),$(document).ready(function(){$(".navTrigger").click(function(){$("nav").toggleClass("open")})});
