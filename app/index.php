1f7f94c6f8e86b7dfe207e5a8cb2145b6261b025
JoeRutland <joe@jrutlanddesign.co.uk>
2016-08-01 14:58:04 +0100

    php pages

--- app/index.php
+++ app/index.php
@@ -14,15 +14,7 @@
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
     
-    <header>
-      <img src="images/suttons-mortgage-brokers.png" alt="Suttons Mortgage Brokers, Yorkshire" class="logo">
-      <div id="topContact">
-        <img src="images/phone-icon.svg" alt="contact Suttons Mortgage Brokers, Yorkshire">
-        <span>01482 376245</span>
-      </div>
-
-      <a href="#" id="menuIcon" class="navTrigger"><img src="images/nav-icon.svg" alt="menu icon - click to expand menu"></a>
-    </header>
+    <?php include('include/header.php'); ?>
 
     <nav>
       <?php include('include/navigation.php'); ?>
