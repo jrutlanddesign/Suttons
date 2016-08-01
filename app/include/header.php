b60f842804f3ff195f07303c4b128376813e24cb
JoeRutland <joe@jrutlanddesign.co.uk>
2016-08-01 14:58:22 +0100

    link up logo within header

---
+++ app/include/header.php
@@ -0,0 +1,9 @@
+<header>
+      <a href="index.php"><img src="images/suttons-mortgage-brokers.png" alt="Suttons Mortgage Brokers, Yorkshire" class="logo"></a>
+      <div id="topContact">
+        <img src="images/phone-icon.svg" alt="contact Suttons Mortgage Brokers, Yorkshire">
+        <span>01482 376245</span>
+      </div>
+
+      <a href="#" id="menuIcon" class="navTrigger"><img src="images/nav-icon.svg" alt="menu icon - click to expand menu"></a>
+    </header>
